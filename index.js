28b147a70374dd27bc5a3a4447f0dd6a0b99403f
Jose Soto <jsoto7087@gmail.com>
2022-05-28 22:26:52 -0400

    commit tree 8ef2bfab601a9e48c049ad513d852eaf7f1caf50

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 // Code your solution in this file!
 
 
-const returnFirstTwoDrivers = function (driversArray) { return driversArray.slice(0, 2)
+const returnFirstTwoDrivers = function (driversArray) {
+  return driversArray.slice(0, 2)
 
 }
-const returnLastTwoDrivers = function (driversArray) {return driversArray.slice(-2)
+const returnLastTwoDrivers = function (driversArray) {
+  return driversArray.slice(-2)
 }
 
-const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers]
-// return selectingDrivers.push(returnFirstTwoDrivers)
- 
-const createFareMultiplier = function (multiplier) {
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
-    return (multiplier * 4)
-//    const newNumber = (num1 * 4);
+function fareQuintupler(multiplier) {
+  return (multiplier * 4)
+};
+
+function createFareMultiplier(multiplier) {
+  return function multiplierReturn(fare) {
+    console.log(fare);
+    return (multiplier * fare)
+  };
+}
+
+function fareDoubler(multiple) {
+  return (multiple * 2)
+}
+
+function fareTripler(number) {
+  return (number * 3);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+  return func(arrayOfDrivers);
+
 }
